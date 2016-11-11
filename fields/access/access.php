31f5eb8eb2759d91305439c49c11578e27d36132
Daniel Weidner <d.weidner@hks-agentur.de>
2016-11-11 16:58:40 +0100

    Use a checkbox instead of a selectbox and allow to save both user and role ids

--- fields/access/access.php
+++ fields/access/access.php
@@ -11,14 +11,14 @@
  * @subpackage AccessField
  * @since      1.0.0
  */
-class AccessField extends SelectField {
+class AccessField extends BaseField {
 
   /**
    * Version of the field.
    *
    * @var string
    */
-  const VERSION = '1.0.0-beta';
+  const VERSION = '1.0.0-beta2';
 
   /**
    * Name of the custom field. Represents the identifier users have to use
@@ -43,26 +43,13 @@ class AccessField extends SelectField {
   ];
 
   /**
-   * List of field properties that can not be changed by the user via the
-   * blueprint.
-   *
-   * @var array<string,mixed>
-   */
-  public static $trumps = [
-    'required' => true,
-    'default'  => 'public',
-  ];
-
-  /**
-   * A list of possible entity types the user can select from, with `public` as
-   * the default.
+   * A list of possible entity types the user can select from.
    *
    * @var array<string>
    */
-  public static $choices = [
-    'public',
-    'users',
+  public static $fields = [
     'roles',
+    'users',
   ];
 
   /**
@@ -78,16 +65,16 @@ class AccessField extends SelectField {
    * @var array<string,array>
    */
   public $exclude = [
+    'roles' => null,
     'users' => null,
-    'roles' => null
   ];
 
   /**
-   * Collection of fields controled by the current instance.
+   * Collection of fields maintained by the current instance.
    *
    * @var array<string,BaseField>
    */
-  protected $children = [];
+  protected $children = null;
 
   /**
    * Load field localization.
@@ -110,16 +97,36 @@ class AccessField extends SelectField {
    */
   public function __construct() {
 
-    parent::__construct();
+    $this->text = $this->l('label');
+    $this->default = true;
 
-    foreach (self::$trumps as $key => $value) {
-      if (property_exists($this, $key)) {
-        $this->{$key} = $value;
-      }
+  }
+
+  /**
+   * Fetch a language variable for multi-language sites. Takes into account the
+   * text domain of the current field.
+   *
+   * @param string $value
+   * @return string
+   */
+  public function l($value) {
+    return l('fields.' . self::FIELDNAME . '.' . $value);
+  }
+
+  /**
+   * Get the id of the current field instance.
+   *
+   * @return string
+   */
+  public function id() {
+
+    $prefix = '';
+
+    if (is_a($this->parentField, 'BaseField')) {
+      $prefix .= $this->parentField->id() . '-';
     }
 
-    $fields = array_slice(self::$choices, 1);
-    $this->children($fields);
+    return $prefix . parent::id();
 
   }
 
@@ -130,78 +137,23 @@ class AccessField extends SelectField {
    */
   public function name() {
 
-    $prefix = ($this->parentField instanceof BaseField) ? $this->parentField->name() . '-' : '';
-    return $prefix . $this->name;
+    $prefix = '';
+
+    if (is_a($this->parentField, 'BaseField')) {
+      $prefix .= $this->parentField->name() . '-';
+    }
+
+    return $prefix . parent::name();
 
   }
 
   /**
-   * Get the current value of the field instance.
+   * Get the default value to use when no value is given by the user.
    *
-   * @return array<string,mixed>
+   * @return mixed
    */
-  public function value() {
-
-    // Expect the field value as yaml formatted string. Provide a default value
-    // if none is given yet.
-    if (is_null($this->value)) {
-      $this->value = [ 'type' => $this->default() ];
-    } else if (is_string($this->value)) {
-      $this->value = yaml::decode($this->value);
-    }
-
-    return $this->value;
-
-  }
-
-  /**
-   * Validate the selected field values.
-   *
-   * @return bool
-   */
-  public function validate() {
-
-    // Retrieve the current field values
-    $data = $this->result(false);
-
-    // Synchronize field value between components
-    $this->sync($data);
-
-    // Validate all child components
-    foreach ($this->children() as $name => $field) {
-      if (!$field->validate()) {
-        return false;
-      }
-    }
-
-    // Validate the current field
-    return array_key_exists(a::get($data, 'type'), $this->options());
-
-  }
-
-  /**
-   * Generate the field value that can be saved within the corresponding content
-   * file. Converts the current value to a yaml formatted string.
-   *
-   * @param bool $encode Whether to yaml encode the result.
-   * @return string
-   */
-  public function result($encode = true) {
-
-    $type = get($this->name());
-
-    if ($this->default() === $type) {
-      $result  = compact('type');
-    } else {
-      $field   = a::get($this->children(), $type);
-      $result  = [
-        'type' => $type,
-        $type  => (array)get($field->name()),
-      ];
-    }
-
-    return $encode ? yaml::encode($result) : $result;
-
+  public function defaultValue() {
+    return ($this->default === '') ? '1' : $this->default;
   }
 
   /**
@@ -219,107 +171,150 @@ class AccessField extends SelectField {
       $this->value = $data;
     }
 
-    foreach ($this->children() as $name => $field) {
+    foreach ($this->fields() as $name => $field) {
       $field->value = a::get($data, $name);
     }
 
   }
 
   /**
-   * Fetch a language variable for multi-language sites. Takes into account the
-   * text domain of the current field.
+   * Collect request data for all fields that belong to the current instance.
    *
-   * @param string $value
+   * @return array<string,array>
+   */
+  public function data() {
+
+    $data = [];
+
+    foreach ($this->fields() as $name => $field) {
+      $data[$name] = get($field->name());
+    }
+
+    return $data;
+
+  }
+
+  /**
+   * Get the current value of the field instance.
+   *
+   * @return mixed
+   */
+  public function value() {
+
+    if ($this->value === '') {
+      $this->value = $this->defaultValue();
+    } else if (is_string($this->value) && !v::accepted($this->value) && !v::denied($this->value)) {
+      $this->value = yaml::decode($this->value);
+    }
+
+    return $this->value;
+
+  }
+
+  /**
+   * Validate the selected field values.
+   *
+   * @return bool
+   */
+  public function validate() {
+
+    // Validate the value of the checkbox
+    $value = get($this->name());
+
+    if (!(is_null($value) || v::accepted($value) || v::denied($value))) {
+      return false;
+    }
+
+    // Validate the value of all fields attached to the current instance
+    $this->sync($this->data());
+
+    foreach ($this->fields() as $name => $field) {
+      if (!$field->validate()) {
+        return false;
+      }
+    }
+
+    // All test have passed. Data can be saved to file.
+    return true;
+  }
+
+  /**
+   * Generate the field value that can be saved within the corresponding content
+   * file. Converts the current value to a yaml formatted string.
+   *
+   * @param bool $encode Whether to yaml encode the result.
    * @return string
    */
-  public function l($value) {
-    return $this->i18n('fields.' . self::FIELDNAME . '.' . $value);
+  public function result($encode = true) {
+
+    $checked = get($this->name());
+
+    // Access control is disabled. Page is public.
+    if (v::accepted($checked)) {
+      return $encode ? '1' : true;
+    }
+
+    // Collect user and role names selected by the user.
+    $data = $this->data();
+    $data = array_filter($data);
+
+    // Access control is enabled. Page is hidden for all.
+    if (empty($data)) {
+      return $encode ? '0' : false;
+    }
+
+    // Access control is enabled. Page is visile for specific users/roles only.
+    return $encode ? yaml::encode($data) : $result;
+
   }
 
   /**
-   * Setup all child elements that belong to the current field.
+   * Test whether the user has enabled access control for the current page.
    *
-   * @return array<string,BaseField>
+   * @return bool
    */
-  public function children($fields = null) {
+  public function checked() {
 
-    if (is_null($fields)) {
-      return $this->children;
-    }
+    $value = $this->value();
+    return !is_array($value) && v::accepted($value);
 
-    $user = panel()->user();
-    $state = $this->value();
+  }
 
-    $defaults = [
-      'page'        => $this->page,
-      'model'       => $this->model,
-      'parentField' => $this,
-      'columns'     => $this->columns(),
-    ];
+  /**
+   * Get the markup for the input element of the field.
+   *
+   * @return \Brick
+   */
+  public function input() {
 
-    foreach ($fields as $name) {
+    $container = brick('label')
+      ->text($this->i18n($this->text()))
+      ->attr('for', $this->id())
+      ->addClass('input')
+      ->addClass('input-with-checkbox');
 
-      $classname = ucfirst($name) . 'Field';
-
-      if (!class_exists($classname) || !$user->can("panel.{$name}.read")) {
-        continue;
-      }
-
-      $instance = new $classname;
-
-      $options = array_merge($defaults, [
-        'name'    => $name,
-        'label'   => $this->l($name),
-        'value'   => a::get($state, $name),
-        'exclude' => a::get($this->exclude, $name, []),
+    $input = brick('input')
+      ->addClass('checkbox')
+      ->addClass('js-toggle-panel')
+      ->attr([
+        'type'          => 'checkbox',
+        'id'            => $this->id(),
+        'name'          => $this->name(),
+        'required'      => $this->required(),
+        'checked'       => $this->checked(),
+        'readonly'      => $this->readonly(),
+        'autofocus'     => $this->autofocus(),
+        'autocomplete'  => $this->autocomplete(),
+        'aria-controls' => $this->id() . '-panel',
+        'aria-expanded' => $this->checked() ? 'false' : 'true',
       ]);
 
-      foreach ($options as $key => $value) {
-        if (property_exists($instance, $key)) {
-          $instance->{$key} = $value;
-        }
-      }
-
-      $this->children[$name] = $instance;
+    if($this->readonly()) {
+      $input->attr('tabindex', '-1');
+      $container->addClass('input-is-readonly');
     }
 
-    return $this->children;
-
-  }
-
-  /**
-   * Generate an option element for each predefined choice.
-   *
-   * @return array<string,string>
-   */
-  public function options() {
-
-    $user = panel()->user();
-    $options  = [];
-
-    foreach (self::$choices as $choice) {
-      if (($this->default() === $choice) || $user->can("panel." . $choice . ".read")) {
-        $options[$choice] = $this->l('type.' . $choice);
-      }
-    }
-
-    return $options;
-
-  }
-
-  /**
-   * Generate the markup for a select option.
-   *
-   * @param mixed $value Value of the option.
-   * @param mixed $text Text displayed for the option.
-   * @param mixed $selected Whether the option is the currently selected one.
-   */
-  public function option($value, $text, $selected = false) {
-
-    return brick('option', $this->i18n($text), [
-      'value'    => $value,
-      'selected' => $selected || ($value === a::get($this->value(), 'type')),
-    ]);
+    return $container->prepend($input);
 
   }
 
@@ -338,6 +333,64 @@ class AccessField extends SelectField {
   }
 
   /**
+   * Get all fields that belong to the current field instance. If the `fields`
+   * paramater is given new fields are added to the footer of the field.
+   *
+   * @return array<string,BaseField>
+   */
+  public function fields() {
+
+    if (!is_null($this->children)) {
+      return $this->children;
+    }
+
+    $fields = [];
+    $state  = $this->value();
+    $user   = panel()->user();
+
+    $defaults = [
+      'page'        => $this->page,
+      'model'       => $this->model,
+      'parentField' => $this,
+      'columns'     => $this->columns(),
+    ];
+
+    foreach (self::$fields as $name) {
+
+      // Ensure the field exists and the user has access to the corresponding
+      // content type
+      $classname = ucfirst($name) . 'Field';
+
+      if (!class_exists($classname) || !$user->can("panel.{$name}.read")) {
+        continue;
+      }
+
+      // Create a new field instance with sensitive defaults
+      $instance = new $classname;
+
+      $options = array_merge($defaults, [
+        'name'    => $name,
+        'label'   => $this->l($name),
+        'value'   => a::get($state, $name),
+        'exclude' => a::get($this->exclude, $name, []),
+      ]);
+
+      foreach ($options as $key => $value) {
+        if (property_exists($instance, $key)) {
+          $instance->{$key} = $value;
+        }
+      }
+
+      // Add the new field to the registry
+      $fields[$name] = $instance;
+
+    }
+
+    return $this->children = $fields;
+
+  }
+
+  /**
    * Generate the markup for the entire field element with all its input
    * elements and accompanying contents.
    *
@@ -346,13 +399,18 @@ class AccessField extends SelectField {
   public function template() {
 
     // Append the markup for all child elements to the container
-    $container = brick('div')->addClass('field-aside');
+    $container = brick('div')
+      ->addClass('field-panel')
+      ->attr([
+        'id' => $this->id() . '-panel',
+      ]);
 
-    // Ensure that the values of all components are synchronized
+    // Ensure that the values of all fields attached to the current instance
+    // are synchronized with the root
     $this->sync();
 
-    // Generate the markup of each sub-component
-    foreach ($this->children() as $name => $field) {
+    // Generate the markup of each field attached
+    foreach ($this->fields() as $name => $field) {
       $type = str_replace('field', '', strtolower(get_class($field)));
       $child = $field->template();
       $child
@@ -365,24 +423,6 @@ class AccessField extends SelectField {
 
     // Generate the markup for the select field and append the child elements.
     return parent::template()->append($container);
-
-  }
-
-  /**
-   * Method overloading. `_call`is triggered whenever an inaccesible method
-   * is invoked.
-   *
-   * @param mixed $name
-   * @param mixed $args
-   */
-  public function __call($name, $args) {
-
-    // Enforce the overrides specified in `self::$trumps`
-    if (array_key_exists($name, self::$trumps)) {
-      return self::$trumps[$name];
-    }
-
-    return isset($this->{$name}) ? $this->{$name} : null;
 
   }
 
